0e5b079f7c4ebe82a6f99344cd6b81dc21099ebb
MahendraN <mahendraramalingam8056@gmail.com>
2024-07-15 20:16:22 +0530

    Update

--- weatherapp/models.py
+++ weatherapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class City(models.Model):
+    name=models.CharField(max_length=25,default="")    # here 'name' is object
+
+    def __str__(self):                                # 'name' is object so convert into string using this function
+        return self.name
 # Create your models here.
